bb74825d0a1a009fc49c4edc55c92c056337e0c7
LucasBiazon <lucasbiazonpalma@gmail.com>
2025-11-01 23:01:43 -0300

    No more error

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,7 +3,6 @@ import authRoutes from './auth.routes.js';
 import coffeesRoutes from './coffees.routes.js';
 import profilesRoutes from './profiles.routes.js';
 import reviewsRoutes from './reviews.routes.js';
-import recommendationsRoutes from './recommendations.routes.js';
 
 const r = Router();
 
@@ -11,6 +10,5 @@ r.use('/auth', authRoutes);
 r.use('/coffees', coffeesRoutes);
 r.use('/profiles', profilesRoutes);
 r.use('/coffees/:id/reviews', reviewsRoutes);
-r.use('/recommendations', recommendationsRoutes);
 
 export default r;
